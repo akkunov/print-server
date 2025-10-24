c02c674a27cfaa4b2ca391de7f4b64a97b8aae1e
Adilet <75267228+akkunov@users.noreply.github.com>
2025-10-24 10:35:17 +0600

    Delete dist directory

--- dist/routes/index.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import uploadRoutes from './upload.route.js';
-import envProfileRoute from "./envPofile.route.js";
-const router = Router();
-router.use('/api', uploadRoutes); // /api/upload
-router.use('/api', envProfileRoute); // /api/upload
-export default router;
